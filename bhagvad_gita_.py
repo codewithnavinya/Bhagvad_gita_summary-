1760b55cbdce3fda73988923fb4308013951fc7a
codewithnavinya <navinyavarute@gmail.com>
2025-07-11 17:28:45 +0530

    Add files via upload

---
+++ bhagvad_gita_.py
@@ -0,0 +1,41 @@
+print('BHAGAVD GITA ALL CHAPATER SUMMARY PROVIDED')
+n=int(input('ENTER THE CHAPATER YOU WANT :'))
+if(n==1):
+         print('Bhagavad Gita Chapter 1 titled "Arjuna Despair" sets the stage for the epic dialogue by introducing the battlefield of Kurukshetra and Arjunas emotional turmoil. It describes the two armies facing each other, with Arjuna, the warrior, overwhelmed by grief and confusion at the prospect of fighting his own kin. He questions the righteousness of war and his role in it, ultimately leading him to seek guidance from Lord Krishna. ')
+elif(n==2):
+             print('lays the foundation for the entire text by introducing the core concepts of the souls immortality, the nature of duty, and the path of selfless action (Karma Yoga).')
+elif(n==3):
+                 print('emphasizes the importance of performing ones duties without attachment to the results')
+elif(n==4):
+       print('discusses the path of knowledge and action, emphasizing selfless service and the importance of understanding the nature of reality.')
+elif(n==5):
+           print('explores the relationship between action (Karma Yoga) and renunciation (Sannyasa)')
+elif(n==6):
+    print('focuses on the practice of controlling the mind and senses to achieve self-realization')
+elif(n==7):
+    print('focuses on the nature of the divine and the path to realizing it')
+elif(n==8):
+    print('focuses on the concept of Brahman, the ultimate reality, and the path to attaining it.')
+elif(n==9):
+    print('emphasizes the supremacy of devotion (Bhakti) as the path to spiritual realization')
+elif(n==10):
+    print('Krishna reveals that neither celestial gods nor great sages fully comprehend His origin, being the source from which they come.')
+elif(n==11):
+    print('describes Arjunas request to witness Krishnas divine, universal form and the subsequent revelation of this awe-inspiring, and terrifying, form')
+elif(n==12):
+    print('focuses on the path of devotion and love for God')
+elif(n==13):
+    print('focuses on the distinction between the field (Kshetra) and the knower of the field (Kshetrajna')
+elif(n==14):
+    print('discusses the three modes of material nature: Sattva (goodness), Rajas (passion), and Tamas (ignorance)')
+elif(n==15):
+    print('describes the material world as an upside-down banyan tree, emphasizing the need for detachment and surrender to Krishna for liberation.')
+elif(n==16):
+    print('discusses the contrasting natures of individuals, highlighting the divine and demoniac qualities')
+elif(n==17):
+    print('discusses the three modes of material nature (Sattva, Rajas, and Tamas) and how they influence faith, worship, austerity, and charity.')
+elif(n==18):
+    print('summarizes the entire text, focusing on renunciation (sannyasa) and relinquishment (tyaga) as paths to liberation (moksha). ')
+else:
+    print('their are only 18 chapater in bhagvad gita')
+    
